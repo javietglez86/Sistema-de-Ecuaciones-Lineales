d4521afeabc7d21158ea8d7a46aa519370a621be
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2016-04-27 20:59:03 -0300

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -19,6 +19,74 @@ public class Matriz {
 		}
 
 	}
+	//metodos sumar overrideados
+
+	public void sumar(Matriz otraMatriz) throws MissMatchDimensionException{
+		try{
+			if (this.getI()==otraMatriz.getI()&&this.getJ()==otraMatriz.getJ()){
+				for (int i = 0; i < this.getI(); i++){
+					for( int j=0; j < this.getJ(); j++){
+						this.matriz[i][j]+=otraMatriz.matriz[i][j];
+					}
+				}
+
+			}
+			else
+				throw new MissMatchDimensionException();
+		}
+		catch(MissMatchDimensionException e){
+
+			System.out.println(e.getTextoError());
+
+		}
+	}
+
+	// RESTAR
+
+	public void restar(Matriz otraMatriz) throws MissMatchDimensionException{
+		for (int i = 0; i < otraMatriz.getI(); i++){
+			for(int j=0; j < otraMatriz.getJ(); j++){
+				otraMatriz.matriz[i][j]*=-1;
+			}
+		}
+
+		this.sumar(otraMatriz);
+	}
+
+	// Productos
+
+	public Matriz producto(Matriz otraMatriz) throws MissMatchDimensionException{
+
+		Matriz matrizResultante = new Matriz(this.getI(), otraMatriz.getJ());
+		Double auxiliar = null;
+
+		try{
+			if (this.getJ()==otraMatriz.getI()){
+				Integer b = 0;
+				for(int i = 0;i<this.getI();i++){
+					Integer a = 0;
+					
+
+					for(int j=0;j<this.getJ();j++){
+						auxiliar+=this.matriz[i][j]*otraMatriz.matriz[a][b];
+						a++;
+					}
+					
+					matrizResultante.matriz[i][b]=auxiliar;
+					b++;
+				}
+			}
+			else
+				throw new MissMatchDimensionException();
+		}
+		catch(MissMatchDimensionException e){
+
+			System.out.println(e.getTextoError());
+
+		}
+		return matrizResultante;
+	}
+
 
 	// Getters y setters
 	public Integer getI() {
